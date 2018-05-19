03fa4b630c89bf26007169cafbd48ff515b24db7
=VictoremWinbringer <V@mail.ru>
2018-05-19 18:55:39 +0300

    init

---
+++ AsynClientServer/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://localhost:8888/");
+                listener.Start();
+                Console.WriteLine("Whait...");
+                while (true)
+                {
+                    HttpListenerContext context = await listener.GetContextAsync();
+                    ThreadPool.QueueUserWorkItem((obj) =>
+                    {
+                        HttpListenerResponse response = context.Response;
+                        string responseString = "Hello";
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                        response.ContentLength64 = buffer.Length;
+                        response.ContentEncoding = Encoding.UTF8;
+                        Stream output = response.OutputStream;
+                        output.Write(buffer, 0, buffer.Length);
+                        output.Close();
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ AsynClientServer/src/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var response = await client.GetAsync("http://localhost:8888/");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine(await response.Content.ReadAsStringAsync());
+                        }
+                        else
+                        {
+                            Console.WriteLine(response.StatusCode.ToString("G"));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+}
